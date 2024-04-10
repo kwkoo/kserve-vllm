74264f47f93a899dbdc33942c03901a0278afeaa
Koo Kin Wai <kkoo@redhat.com>
2024-04-10 20:08:47 +0800

    Index documents from an S3 bucket instead of a local PV

--- rag/frontend/app/ingest.py
+++ rag/frontend/app/ingest.py
@@ -1,79 +1,56 @@
 #!/usr/bin/env python3
 import os
-import glob
 from typing import List, AsyncIterable
 import asyncio
 import functools
 from db import get_db_connection, get_existing_sources
-
-from langchain.document_loaders import (
-    CSVLoader,
-    EverNoteLoader,
-    PyMuPDFLoader,
-    TextLoader,
-    UnstructuredEPubLoader,
-    UnstructuredHTMLLoader,
-    UnstructuredMarkdownLoader,
-    UnstructuredODTLoader,
-    UnstructuredPowerPointLoader,
-    UnstructuredWordDocumentLoader,
-)
-
+from langchain_community.document_loaders import S3FileLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings import HuggingFaceEmbeddings
+import boto3
 
 
 # Load environment variables
-source_directory = os.environ.get('SOURCE_DIRECTORY', 'source_documents')
+bucket_name = os.environ.get("SOURCE_BUCKET", "documents")
 chunk_size = 500
 chunk_overlap = 50
 
+supported_extensions = ['.csv', '.doc', '.docx', '.epub', '.html', '.md', '.odt', '.pdf', '.ppt', '.pptx', '.txt']
 
-# Map file extensions to document loaders and their arguments
-LOADER_MAPPING = {
-    ".csv": (CSVLoader, {}),
-    ".doc": (UnstructuredWordDocumentLoader, {}),
-    ".docx": (UnstructuredWordDocumentLoader, {}),
-    ".enex": (EverNoteLoader, {}),
-    ".epub": (UnstructuredEPubLoader, {}),
-    ".html": (UnstructuredHTMLLoader, {}),
-    ".md": (UnstructuredMarkdownLoader, {}),
-    ".odt": (UnstructuredODTLoader, {}),
-    ".pdf": (PyMuPDFLoader, {}),
-    ".ppt": (UnstructuredPowerPointLoader, {}),
-    ".pptx": (UnstructuredPowerPointLoader, {}),
-    ".txt": (TextLoader, {"encoding": "utf8"}),
-    # Add more mappings for other file extensions and loaders as needed
-}
+def get_file_list() -> List[str]:
+    session = boto3.session.Session()
+    client = session.client(service_name='s3')
+    resp = client.list_objects_v2(Bucket=bucket_name)
+    if resp is None or resp.get('Contents') is None:
+        return []
+
+    contents = resp.get('Contents')
+    return [f.get('Key') for f in contents]
 
 class Ingester:
     texts = None
 
-    def __init__(self, source_directory) -> None:
-        self.source_directory = source_directory
+    def __init__(self, bucket_name) -> None:
+        self.bucket_name = bucket_name
 
     async def load_documents_and_split(self, ignored_files: List[str] = []) -> AsyncIterable[str]:
         """
         Loads all documents from the source documents directory, ignoring specified files
         """
         loop = asyncio.get_event_loop()
-        yield(f"Loading documents from {self.source_directory}\n")
-        all_files = []
-        for ext in LOADER_MAPPING:
-            all_files.extend(
-                glob.glob(os.path.join(self.source_directory, f"**/*{ext}"), recursive=True)
-            )
-        filtered_files = [file_path for file_path in all_files if file_path not in ignored_files]
+        yield(f"Loading documents from {self.bucket_name}\n")
+        filtered_files = []
+        for f in get_file_list():
+            if f"s3://{self.bucket_name}/{f}" not in ignored_files:
+                filtered_files.append(f)
         total_files = len(filtered_files)
         documents = []
         for i, file_path in enumerate(filtered_files):
             ext = "." + file_path.rsplit(".", 1)[-1]
-            if ext not in LOADER_MAPPING:
+            if ext not in supported_extensions:
                 yield(f"Unsupported file extension '{ext}'\n")
                 continue
             yield(f"Loading {file_path} ({i+1} / {total_files})\n")
-            loader_class, loader_args = LOADER_MAPPING[ext]
-            loader = loader_class(file_path, **loader_args)
+            loader = S3FileLoader(self.bucket_name, file_path)
             load_result = await loop.run_in_executor(None, loader.load)
             documents.extend(load_result)
 
@@ -81,14 +58,14 @@ class Ingester:
             yield("Did not load any documents\n")
             return
 
-        yield(f"Loaded {len(documents)} new documents from {source_directory}\n")
+        yield(f"Loaded {len(documents)} new documents from {self.bucket_name}\n")
         text_splitter = await loop.run_in_executor(None, functools.partial(RecursiveCharacterTextSplitter, chunk_size=chunk_size, chunk_overlap=chunk_overlap))
         texts = await loop.run_in_executor(None, text_splitter.split_documents, documents)
         yield(f"Split into {len(texts)} chunks of text (max. {chunk_size} tokens each)\n")
         self.texts = texts
 
 async def ingest_documents() -> AsyncIterable[str]:
-    ingester = Ingester(source_directory)
+    ingester = Ingester(bucket_name)
 
     ignored_files = get_existing_sources()
     if len(ignored_files) > 0:
