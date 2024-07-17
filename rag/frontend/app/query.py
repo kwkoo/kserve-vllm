1e7f9b16d480011d27d0028cbf7e97e4ce249802
Koo Kin Wai <kin.wai.koo@gmail.com>
2024-07-17 14:34:44 +0800

    Reranking support

--- rag/frontend/app/query.py
+++ rag/frontend/app/query.py
@@ -1,16 +1,29 @@
 #!/usr/bin/env python3
 from langchain.chains import RetrievalQA
+from langchain.retrievers import ContextualCompressionRetriever
+from langchain.retrievers.document_compressors import CrossEncoderReranker
+from langchain_community.cross_encoders import HuggingFaceCrossEncoder
 from langchain.callbacks import AsyncIteratorCallbackHandler
 from langchain_openai import OpenAI
 
+import logging
+import sys
 import json
 import os
 from typing import AsyncIterable, Any
 import asyncio
 from db import get_db_connection
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+stream_handler = logging.StreamHandler(sys.stdout)
+log_formatter = logging.Formatter("%(asctime)s [%(processName)s: %(process)d] [%(threadName)s: %(thread)d] [%(levelname)s] %(name)s: %(message)s")
+stream_handler.setFormatter(log_formatter)
+logger.addHandler(stream_handler)
+
 
 model = os.environ.get("MODEL", "/mnt/models")
+reranker_model_name = os.environ.get("RERANKER_MODEL_NAME", "BAAI/bge-reranker-base")
 openai_api_base = os.environ.get("OPENAI_API_BASE", "http://localhost:8080/v1")
 openai_api_key = os.environ.get("OPENAI_API_KEY", "EMPTY")
 target_source_chunks = int(os.environ.get('TARGET_SOURCE_CHUNKS',4))
@@ -19,12 +32,34 @@ retriever = None
 llm = None
 
 
+def parse_boolean_environent_variable(key: str, default=False) -> bool:
+    env_var = os.getenv(key)
+    if env_var is None:
+        return default
+    env_var = env_var.lower()
+    val = default
+    if env_var == '0' or env_var == 'false' or env_var == 'no' or env_var == 'n':
+        val = False
+    elif env_var == '1' or env_var == 'true' or env_var == 'yes' or env_var == 'y':
+        val = True
+    return val
 
 def initialize_query_engine():
     global retriever
 
     db = get_db_connection()
-    retriever = db.as_retriever(search_kwargs={"k": target_source_chunks})
+    db_retriever = db.as_retriever(search_kwargs={"k": target_source_chunks})
+    retriever = db_retriever
+
+    if parse_boolean_environent_variable('USE_RERANKER'):
+        logger.info('using reranker')
+        reranker_model = HuggingFaceCrossEncoder(model_name=reranker_model_name)
+        compressor = CrossEncoderReranker(model=reranker_model, top_n=3)
+        retriever = ContextualCompressionRetriever(
+            base_compressor=compressor, base_retriever=db_retriever
+        )
+    else:
+        logger.info('not using reranker')
 
 async def llm_query(prompt: str) -> AsyncIterable[str]:
     callback = AsyncIteratorCallbackHandler()
@@ -66,7 +101,7 @@ async def llm_query(prompt: str) -> AsyncIterable[str]:
 initialize_query_engine()
 
 async def main():
-    async for token in llm_query('how do you install openshift'):
+    async for token in llm_query('describe the dip switch settings for the 2-axis servo amplifier'):
         print(token)
 
 if __name__ == "__main__":
